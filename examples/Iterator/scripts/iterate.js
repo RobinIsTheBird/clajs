772448ee40c3600595a8311a7951ef68aec13fc4
Robin Schaufler <robin@likethebird.com>
2014-10-26 16:34:07 -0400

    Test all three iterators.
    base Iterator & StepIterator pass.
    RandomIterator fails, apparently due to a bug in clajs.

--- examples/Iterator/scripts/iterate.js
+++ examples/Iterator/scripts/iterate.js
@@ -6,12 +6,42 @@ var limit = 10;
 if (2 < process.argv.length) {
     limit = parseFloat(process.argv[2]);
 }
-console.log('limit = ' + limit);
+console.log('limit = \n' + limit);
 
 var Iterator = require('./Iterator.js');
+var StepIterator = require('./StepIterator.js');
+var RandomIterator = require('./RandomIterator.js');
 
 var anIterator = new Iterator();
 
+console.log('\nBase Iterator:\n');
+console.log('Cur: ' + anIterator.cur);
+for (var i = limit; 0 < i--;) {
+    console.log('Next: ' + anIterator.next());
+    console.log('Cur: ' + anIterator.cur);
+}
+
+anIterator = new StepIterator({
+    step: 5
+});
+
+console.log('\nStep Iterator:\n');
+console.log('Cur: ' + anIterator.cur);
+for (var i = limit; 0 < i--;) {
+    if (i === Math.floor(limit / 2)) {
+        console.log('\nStep: ' + anIterator.step);
+        anIterator.step = 2;
+        console.log('New Step: ' + anIterator.step + '\n');
+    }
+    console.log('Next: ' + anIterator.next());
+    console.log('Cur: ' + anIterator.cur);
+}
+
+anIterator = new RandomIterator({
+    ceil: 25
+});
+
+console.log('\nRandom Iterator:\n');
 console.log('Cur: ' + anIterator.cur);
 for (var i = limit; 0 < i--;) {
     console.log('Next: ' + anIterator.next());
